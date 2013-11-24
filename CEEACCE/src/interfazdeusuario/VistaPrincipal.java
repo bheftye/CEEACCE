a44f34c28a5a22d2ce58d10c329e84c2fe03622a
goregeorge <aries.george@hotmail.com>
2013-11-24 13:51:30 -0600

    Ya no disposea

--- CEEACCE/src/interfazdeusuario/VistaPrincipal.java
+++ CEEACCE/src/interfazdeusuario/VistaPrincipal.java
@@ -109,7 +109,6 @@ public class VistaPrincipal extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         new VistaAdministradorAlumnos().setVisible(true);
-        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -121,13 +120,11 @@ public class VistaPrincipal extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
         new VistaAdministradorDeCalificaciones().setVisible(true);
-        this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         new VistaAdministradorDeCursos().setVisible(true);
-        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**

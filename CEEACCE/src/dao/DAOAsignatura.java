dc2f27da0942f5a972fcaa22bca6a54403929118
goregeorge <aries.george@hotmail.com>
2013-11-30 15:38:56 -0600

    DAOAsignatura, método insertar

--- CEEACCE/src/dao/DAOAsignatura.java
+++ CEEACCE/src/dao/DAOAsignatura.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import modelo.Asignatura;
 import java.util.ArrayList;
 
@@ -14,7 +17,28 @@ public class DAOAsignatura extends DAO<Asignatura> {
 
     @Override
     public int insertar(Asignatura asignatura) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String nombreAsignatura = asignatura.getNombreAsignatura();
+        int calificacion = asignatura.getCalificacion();
+        boolean esSerializada = asignatura.isSerializacion();
+        String maestroImparteAsignatura = asignatura.getMaestroQueImparte();
+        String fechaImparticion = asignatura.getFechaImparticion();
+        int creditosAsignatura = asignatura.getCreditos();
+        int duracionAsignatura = asignatura.getDuracion();
+        String queryInsercion = "INSERT INTO asignatura (nomasig,calificacion, serializacion, maestro, fechaimparticion, creditos, duracion)"
+                + " VALUES ("+nombreAsignatura+","+calificacion+","+esSerializada+","+maestroImparteAsignatura+","+fechaImparticion+","+creditosAsignatura+","+duracionAsignatura+")";
+        int numFilasAfectadas = 0;
+        Connection conexion = getConexion();
+        try{
+        Statement sentencia = conexion.createStatement();
+        numFilasAfectadas = sentencia.executeUpdate(queryInsercion);
+        sentencia.close();
+        }catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }catch(Exception exception){
+            exception.printStackTrace();
+        }
+        cerrarConexion(conexion);
+        return numFilasAfectadas;
     }
 
     @Override

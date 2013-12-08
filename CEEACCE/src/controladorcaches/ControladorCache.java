14381d2bbc61a64fe1c9719892ae4eaadfebb6c6
pedroharri <pedro_uaz_alumno@hotmail.com>
2013-12-08 14:21:31 -0600

    llenarListaCuros y Obtener Curso

--- CEEACCE/src/controladorcaches/ControladorCache.java
+++ CEEACCE/src/controladorcaches/ControladorCache.java
@@ -7,6 +7,8 @@ package controladorcaches;
 import controladordepeticiones.ControladorDePeticiones;
 import java.util.ArrayList;
 import modelo.Alumno;
+import modelo.Curso;
+import modelo.ListaDeCursos;
 import modelo.ListaDePlanesDeEstudio;
 import modelo.ListaDeUsuarios;
 import modelo.PlanDeEstudio;
@@ -17,32 +19,35 @@ import modelo.Usuario;
  * @author brentheftye
  */
 public class ControladorCache {
+
     private static final ControladorCache controladorCache = new ControladorCache();
-    
-    
-    private ControladorCache(){}
-    
-    public static ControladorCache getControladorCache(){
+
+    private ControladorCache() {
+    }
+
+    public static ControladorCache getControladorCache() {
         return controladorCache;
     }
-    
-    public void llenarListaDeAlumnos(){
+
+    public void llenarListaDeAlumnos() {
         ArrayList<Alumno> alumnos = ControladorDePeticiones.getControladorDePeticiones().obtenerAlumnos();
-        
+
     }
-    
-    public void llenarListaDePlanesDeEstudio(){
+
+    public void llenarListaDePlanesDeEstudio() {
         ArrayList<PlanDeEstudio> planesDeEstudio = ControladorDePeticiones.getControladorDePeticiones().obtenerPlanesDeEstudio();
         ListaDePlanesDeEstudio.getListaDePlanesDeEstudio().setPlanesDeEstudio(planesDeEstudio);
     }
-    
-    public void llenarListaDeCursos(){
-    
+
+    public void llenarListaDeCursos(String nombreCurso) {
+        ArrayList<Curso> curso = ControladorDePeticiones.getControladorDePeticiones().obtenerCurso(nombreCurso);
+        ListaDeCursos.getListaDeCursos().setCursos(curso);
+        
+
     }
-    
-    public void llenarListaDeUsuarios(){
+
+    public void llenarListaDeUsuarios() {
         ArrayList<Usuario> usuarios = ControladorDePeticiones.getControladorDePeticiones().obtenerUsuarios();
         ListaDeUsuarios.getListaDeUsuarios().setUsuarios(usuarios);
     }
-    
 }
--- CEEACCE/src/controladordepeticiones/ControladorDePeticiones.java
+++ CEEACCE/src/controladordepeticiones/ControladorDePeticiones.java
@@ -22,107 +22,114 @@ import modelo.Usuario;
  * @author brentheftye
  */
 public class ControladorDePeticiones {
-    private static ControladorDePeticiones  controladorDePeticiones = new ControladorDePeticiones();
-    
-    private ControladorDePeticiones(){}
-    
-    public static ControladorDePeticiones getControladorDePeticiones(){
-    return controladorDePeticiones;
+
+    private static ControladorDePeticiones controladorDePeticiones = new ControladorDePeticiones();
+
+    private ControladorDePeticiones() {
     }
-    
-    public boolean agregarAlumno(Alumno alumno){
+
+    public static ControladorDePeticiones getControladorDePeticiones() {
+        return controladorDePeticiones;
+    }
+
+    public boolean agregarAlumno(Alumno alumno) {
         int numeroFilasAfectadas = DAOAlumno.getDAOAlumno().insertar(alumno);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public boolean modificarAlumno(Alumno alumno){
+
+    public boolean modificarAlumno(Alumno alumno) {
         int numeroFilasAfectadas = DAOAlumno.getDAOAlumno().actualizar(alumno);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public boolean agregarPlanDeEstudio(PlanDeEstudio planDeEstudio){
+
+    public boolean agregarPlanDeEstudio(PlanDeEstudio planDeEstudio) {
         int numeroFilasAfectadas = DAOPlanDeEstudio.getDAOPlanDeEstudio().insertar(planDeEstudio);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public boolean agregarCurso(Curso curso){
+
+    public boolean agregarCurso(Curso curso) {
         int numeroFilasAfectadas = DAOCurso.getDAOCurso().insertar(curso);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public boolean darAltaUsuario(Usuario usuario){
+
+    public boolean darAltaUsuario(Usuario usuario) {
         int numeroFilasAfectadas = DAOUsuario.getDAOUsuario().insertar(usuario);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public ArrayList<Usuario> obtenerUsuarios(){
+
+    public ArrayList<Usuario> obtenerUsuarios() {
         return DAOUsuario.getDAOUsuario().consultar("select * from usuario order by clvusuario asc");
     }
-    
-    public boolean agregarAsignatura(Asignatura nuevaAsignatura){
+
+    public ArrayList<Curso> obtenerCurso(String nombreCurso) {
+        return DAOCurso.getDAOCurso().consultar("select * from curso WHERE nomCurso = '" + nombreCurso + "'");
+
+    }
+
+    public boolean agregarAsignatura(Asignatura nuevaAsignatura) {
         int numeroFilasAfectadas = DAOAsignatura.getDAOAsignatura().insertar(nuevaAsignatura);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public boolean registraAsignaturaEnPlanDeEstudio(int clavePlanDeEstudio, int modulo, String claveAsignatura ){
-        String queryDeRegistro = "INSERT INTO planmoduloasignatura (clvplan, clvmodulo,clvasign) VALUES ("+clavePlanDeEstudio+","+modulo+",'"+claveAsignatura+"')";
+
+    public boolean registraAsignaturaEnPlanDeEstudio(int clavePlanDeEstudio, int modulo, String claveAsignatura) {
+        String queryDeRegistro = "INSERT INTO planmoduloasignatura (clvplan, clvmodulo,clvasign) VALUES (" + clavePlanDeEstudio + "," + modulo + ",'" + claveAsignatura + "')";
         int numeroFilasAfectadas = DAOAsignatura.getDAOAsignatura().ejecutaQuery(queryDeRegistro);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public int obtenClaveDePlanDeEstudioPorNombre(String nombrePlanDeEstudio){
-        String queryDeConsulta = "SELECT * FROM plandeestudio WHERE nomplan = '"+nombrePlanDeEstudio+"'";
+
+    public int obtenClaveDePlanDeEstudioPorNombre(String nombrePlanDeEstudio) {
+        String queryDeConsulta = "SELECT * FROM plandeestudio WHERE nomplan = '" + nombrePlanDeEstudio + "'";
         return DAOPlanDeEstudio.getDAOPlanDeEstudio().obtenerClaveDePlanDeEstudioPorNombre(queryDeConsulta);
     }
-    
-    public boolean eliminarPlanDeEstudio(PlanDeEstudio planDeEstudio){
+
+    public boolean eliminarPlanDeEstudio(PlanDeEstudio planDeEstudio) {
         int numeroFilasAfectadas = DAOPlanDeEstudio.getDAOPlanDeEstudio().eliminar(planDeEstudio);
-        if(numeroFilasAfectadas > 0){
+        if (numeroFilasAfectadas > 0) {
             return true;
         }
         return false;
     }
-    
-    public ArrayList<PlanDeEstudio> obtenerPlanesDeEstudio(){
+
+    public ArrayList<PlanDeEstudio> obtenerPlanesDeEstudio() {
         int NUM_DE_MODULOS = 6;
         String queryDeConsulta = "select * from plandeestudio order by clvplan asc ";
-        ArrayList<PlanDeEstudio> planesDeEstudio= DAOPlanDeEstudio.getDAOPlanDeEstudio().consultar(queryDeConsulta);
+        ArrayList<PlanDeEstudio> planesDeEstudio = DAOPlanDeEstudio.getDAOPlanDeEstudio().consultar(queryDeConsulta);
         for (int i = 0; i < planesDeEstudio.size(); i++) {
             PlanDeEstudio planDeEstudioIndexado = planesDeEstudio.get(i);
             for (int j = 0; j < NUM_DE_MODULOS; j++) {
                 Modulo moduloIndexado = planDeEstudioIndexado.getModulos().get(j);
-                String queryDeAsignaturasDeModulo = "select clvasig,nomasig,serializacion,creditos,duracion from planmoduloasignatura,asignatura where clvplan = "+planDeEstudioIndexado.getClave()+" and clvmodulo = "+(j+1)+" and clvasign = clvasig";
+                String queryDeAsignaturasDeModulo = "select clvasig,nomasig,serializacion,creditos,duracion from planmoduloasignatura,asignatura where clvplan = " + planDeEstudioIndexado.getClave() + " and clvmodulo = " + (j + 1) + " and clvasign = clvasig";
                 moduloIndexado.setAsignaturas(DAOAsignatura.getDAOAsignatura().consultar(queryDeAsignaturasDeModulo));
             }
         }
         return planesDeEstudio;
     }
-    
-    public ArrayList<Alumno> obtenerAlumnos(){
+
+    public ArrayList<Alumno> obtenerAlumnos() {
         return null;
     }
-    
+
     public PlanDeEstudio obtenerNuevoPlanDeEstudioPorClave(int clavePlanDeEstudio) {
         int NUM_DE_MODULOS = 6;
         String queryDeConsulta = "select * from plandeestudio where clvplan =" + clavePlanDeEstudio;
@@ -137,4 +144,4 @@ public class ControladorDePeticiones {
         }
         return null;
     }
-}   
+}
